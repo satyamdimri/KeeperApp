b2d0f8f0ca34d18e4d12cfbd9031dab12da0691c
SATYAM DIMRI <157374020+satyamdimri@users.noreply.github.com>
2025-06-11 23:54:34 +0530

    Updated app.jsx

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,11 +2,23 @@ import React from "react";
 import Header from "./header";
 import Footer from "./footer";
 import Note from "./note";
+import notes from "../notes";
+
+function createNotes(noteItem) {
+  return <Note title={noteItem.title} content={noteItem.content} />;
+}
+
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map((noteItem) => (
+        <Note
+          key={noteItem.key}
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+      ))}
       <Footer />
     </div>
   );
