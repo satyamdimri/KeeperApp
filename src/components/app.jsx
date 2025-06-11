db7400b75960ee34e93b3dfe9e353a2105e7f6af
SATYAM DIMRI <157374020+satyamdimri@users.noreply.github.com>
2025-06-12 00:03:22 +0530

    Updated app.jsx

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,26 +1,44 @@
-import React from "react";
-import Header from "./header";
-import Footer from "./footer";
-import Note from "./note";
-import notes from "../notes";
-
-function createNotes(noteItem) {
-  return <Note title={noteItem.title} content={noteItem.content} />;
-}
+import React, { useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
     <div>
       <Header />
-      {notes.map((noteItem) => (
-        <Note
-          key={noteItem.key}
-          title={noteItem.title}
-          content={noteItem.content}
-        />
-      ))}
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
       <Footer />
     </div>
   );
 }
+
 export default App;
